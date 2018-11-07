56e37e63549c20bdc5fcfccb11a97492ef1b20c9
Uri Pasternak <uri.pasternak@gmail.com>
2018-11-07 11:34:10 +0200

    change the api gateway Url

--- webapp/js/config.js
+++ webapp/js/config.js
@@ -5,7 +5,7 @@ window._config = {
         region: 'us-east-1'
     },
     api: {
-        invokeUrl: 'https://ynlwx46318.execute-api.us-east-1.amazonaws.com/dev'
+        invokeUrl: 'https://qesutvhl79.execute-api.us-east-1.amazonaws.com/dev/'
     }
 };
 
