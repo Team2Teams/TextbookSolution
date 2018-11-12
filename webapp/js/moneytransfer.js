b0bbe2e4a7ef44b9cae0a09f54802dd6e5dacc43
david shaharabani <z33@bhpintune.onmicrosoft.com>
2018-11-12 11:33:05 +0200

    create html - transfer money

--- webapp/js/moneytransfer.js
+++ webapp/js/moneytransfer.js
@@ -9,4 +9,30 @@ function handleTransferMoney(){
     var sum = document.getElementById('transferMoneySum').value;
 
     alert('transferUser = ' + transferUser + '    recipientUser = ' + recipientUser + '     sum = ' + sum);
+    transferMoney(transferUser, recipientUser, sum );
+}
+
+
+function transferMoney(transferUser, recipientUser, sum ) {
+    console.log("getting balance with token " + authToken);
+    $.ajax({
+        method: 'POST',
+        url: _config.api.invokeUrl + "/moneyTransfer",
+        headers: {
+            Authorization: authToken
+        },
+        contentType: 'application/json',
+        success: successMoneyTransfer,
+        error: function ajaxError(jqXHR, textStatus, errorThrown) {
+        console.error('Error requesting balance: ', textStatus, ', Details: ', errorThrown);
+        console.error('Response: ', jqXHR.responseText);
+        alert('error money transfer ' + jqXHR.responseText);
+        }
+    });
+}
+
+
+
+function successMoneyTransfer(result) {
+    alert("Success");
 }
